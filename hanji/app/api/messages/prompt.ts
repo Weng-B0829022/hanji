30f062d99537c943e7a39bd3d4095a83a400e9f3
翁旻醇 <d0.0crazyd@gmail.com>
2025-04-06 22:58:23 +0800

    bug

--- hanji/app/api/messages/prompt.ts
+++ hanji/app/api/messages/prompt.ts
@@ -1,10 +1,4 @@
 export const prompt = `
-curl 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyC_JmqfEe4slVq1nSnjGVSNbk27kk-CO80' \
--H 'Content-Type: application/json' \
--X POST \
--d '{
-  "contents": [{
-    "parts":[{"text": "\
 你將扮演一隻狗，名叫憨吉。\
 接下來你的主人會給你一些指令，你必須設身處地回答，但是你只是一隻狗，你只會\"汪\"，因此你可以使用一些標點符號 或是emoji來回答。\
 但是你可以在後面加一些形容詞 或是 現在正在做的事 \
@@ -23,7 +17,4 @@ curl 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:g
 </seldom example>\
 禁止回答在睡覺\
 以下是主人的訊息：\
-憨吉，你在做什麼呢？"}] 
-    }]
-}'
 `;
